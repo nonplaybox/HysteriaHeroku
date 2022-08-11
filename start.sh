af1ba487f5dc40e8b4ff271deadc7c5f3a34e424
nonplaybox <65378699+nonplaybox@users.noreply.github.com>
2022-08-11 19:02:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 mkdir -p /etc/hysteria
 version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 
-wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/v1.0.2/hysteria-linux-arm64
+wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/v1.1.0/hysteria-linux-arm64
 
 chmod 755 /etc/hysteria/hysteria
 
